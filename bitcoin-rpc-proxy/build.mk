dc0197f6ab6f6f229da928af115437e7e30f1a1d
Martin Habovstiak <martin.habovstiak@gmail.com>
2019-10-11 15:23:01 +0200

    Fixed man page generation for rpc proxy

--- bitcoin-rpc-proxy/build.mk
+++ bitcoin-rpc-proxy/build.mk
@@ -4,7 +4,7 @@ btc-rpc-proxy:
 	git clone https://github.com/Kixunil/btc-rpc-proxy
 
 btc-rpc-proxy/target/debian/btc-rpc-proxy_$(BTC_RPC_PROXY_VERSION)_$(DEB_ARCH).deb: btc-rpc-proxy
-	cd btc-rpc-proxy; cargo deb
+	cd btc-rpc-proxy; MAN_DIR=target/man cargo deb
 
 clean_bitcoin_rpc_proxy:
 	rm -rf btc-rpc-proxy
