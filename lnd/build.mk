9c4f6a4cc202d2ebc93f4d2b59bcdd8cd8e63fa9
Martin Habovstiak <martin.habovstiak@gmail.com>
2020-06-11 19:08:55 +0200

    Use debcrfter-native alternatives
    This change switches to use the new feature of debcrafter for
    implementing alternatives. This should be more robust as it avoids
    conflicts between scripts and the logic of debcrfter should keep the
    invocation consistent.

--- lnd/build.mk
+++ lnd/build.mk
@@ -2,17 +2,11 @@ include $(LND_SOURCE_DIR)$(LND_BUILD_STRATEGY).mk
 
 LND_BUILD_DIR=$(BUILD_DIR)/lnd-$(LND_VERSION)
 
-$(LND_BUILD_DIR)/debian/lncli.postinst: $(LND_SOURCE_DIR)/assets/debian/lncli.postinst $(BUILD_DIR)/repository.stamp
-	cp $< $@
-
-$(LND_BUILD_DIR)/debian/lncli.prerm: $(LND_SOURCE_DIR)/assets/debian/lncli.prerm $(BUILD_DIR)/repository.stamp
-	cp $< $@
-
 $(LND_BUILD_DIR)/xlncli: $(LND_SOURCE_DIR)/xlncli
 	cp $< $@
 	chmod +x $@
 
-$(BUILD_DIR)/lnd.stamp: $(BUILD_DIR)/repository.stamp $(LND_BUILD_DIR)/xlncli $(LND_DEPS) $(addprefix $(LND_BUILD_DIR)/debian/,lncli.postinst lncli.prerm)
+$(BUILD_DIR)/lnd.stamp: $(BUILD_DIR)/repository.stamp $(LND_BUILD_DIR)/xlncli $(LND_DEPS)
 	cd $(LND_BUILD_DIR) && dpkg-buildpackage -a $(DEB_ARCH) $(BUILD_PACKAGE_FLAGS)
 	touch $@
 
--- pkg_specs/lncli.sps
+++ pkg_specs/lncli.sps
@@ -8,3 +8,13 @@ long_doc = """lncli is a tool used for managing lnd from the command line. This 
 also contains a wrapper used to connect to the system-wide LND by default.
 You can still use --macaroonpath --rpcserver and --tlscertpath to access a
 different lnd."""
+
+[alternatives."/usr/share/lncli/xlncli"]
+name = "lncli"
+dest = "/usr/bin/lncli"
+priority = 100
+
+[alternatives."/usr/lib/lncli/lncli"]
+name = "lncli"
+dest = "/usr/bin/lncli"
+priority = 50
