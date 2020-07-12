d46ebc1d81be97a938c3def69e5615e039e4bdf2
Martin Habovstiak <martin.habovstiak@gmail.com>
2020-07-12 20:47:02 +0200

    Make unlocker test network-independent
    This makes test inspect `$0` to determine the network name and test the
    appropriate package.

--- tests/per-package/lnd-unlocker-system-mainnet/after_install.sh
+++ tests/per-package/lnd-unlocker-system-mainnet/after_install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+network="`echo -n $0 | sed 's/^.*-\([^-\/]*\)\/after_install\.sh$/\1/'`"
+
+if echo "$network" | grep -q '^[a-z]\+$';
+then
+	echo -n
+else
+	echo "Can't determine network from argument: $0"
+	exit 1
+fi
+
+port="`grep '^restlisten=' /etc/lnd-system-$network/lnd.conf | sed 's/^restlisten=[^:]*://'`" || exit 1
+
 assert_exists() {
 	if sudo [ '!' -e "$1" ];
 	then
@@ -9,26 +21,28 @@ assert_exists() {
 }
 
 getinfo() {
-	wget -O /dev/null --ca-certificate /var/lib/lnd-system-mainnet/public/tls.cert --header "Grpc-Metadata-macaroon: `sudo xxd -ps -u -c 1000 /var/lib/lnd-system-mainnet/private/admin.macaroon`" https://127.0.0.1:9090/v1/getinfo
+	wget -O /dev/null --ca-certificate /var/lib/lnd-system-$network/public/tls.cert --header "Grpc-Metadata-macaroon: `sudo xxd -ps -u -c 1000 /var/lib/lnd-system-$network/private/admin.macaroon`" https://127.0.0.1:$port/v1/getinfo
 	return $?
 }
 
-assert_exists /var/lib/lnd-system-mainnet/.seed.txt
-assert_exists /var/lib/lnd-system-mainnet/private/admin.macaroon
-assert_exists /var/lib/lnd-system-mainnet/readonly/readonly.macaroon
-assert_exists /var/lib/lnd-system-mainnet/invoice/invoice.macaroon
+sleep 20
 
-if sudo ls -l /var/lib/lnd-system-mainnet/private/admin.macaroon | grep -q '^-rw-r----- ';
+assert_exists /var/lib/lnd-system-$network/.seed.txt
+assert_exists /var/lib/lnd-system-$network/private/admin.macaroon
+assert_exists /var/lib/lnd-system-$network/readonly/readonly.macaroon
+assert_exists /var/lib/lnd-system-$network/invoice/invoice.macaroon
+
+if sudo ls -l /var/lib/lnd-system-$network/private/admin.macaroon | grep -q '^-rw-r----- ';
 then
 	echo 'Permissions OK'
 else
-	echo 'Bad permissions on /var/lib/lnd-system-mainnet/private/admin.macaroon:'
-	ls -l /var/lib/lnd-system-mainnet/private/admin.macaroon
+	echo 'Bad permissions on /var/lib/lnd-system-$network/private/admin.macaroon:'
+	ls -l /var/lib/lnd-system-$network/private/admin.macaroon
 	sudo exit 1
 fi
 
 # TODO: parse as json and check it's an array of 12 strings
-if [ `sudo cat /var/lib/lnd-system-mainnet/.seed.txt | wc -c` -eq 0 ];
+if [ `sudo cat /var/lib/lnd-system-$network/.seed.txt | wc -c` -eq 0 ];
 then
 	echo 'Error: Seed is empty'
 	exit 1
@@ -36,13 +50,13 @@ fi
 
 getinfo || exit 1
 
-sudo systemctl stop lnd-system-mainnet
+sudo systemctl stop lnd-system-$network
 sleep 5
 
 # Make sure the service stopped
 getinfo && exit 1
 
-sudo systemctl start lnd-system-mainnet
+sudo systemctl start lnd-system-$network
 sleep 15
 
 getinfo || exit 1
