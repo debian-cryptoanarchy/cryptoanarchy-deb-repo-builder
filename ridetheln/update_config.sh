e540c414e279475cf74bb7d6108e920378743be3
Martin Habovstiak <martin.habovstiak@gmail.com>
2020-07-29 16:41:40 +0200

    Removed making directories of RTL
    The reason why making those directories was necessary was a bug in
    debcrafter. It was fixed recently so those lines are no longer needed.

--- ridetheln/update_config.sh
+++ ridetheln/update_config.sh
@@ -4,15 +4,6 @@ cfg_file="/etc/ridetheln-system/rtl.conf"
 rt_file="/var/lib/ridetheln-system/RTL-Config.json"
 rt_temp="$rt_file.tmp"
 
-rt_dir="`dirname "$rt_file"`"
-mkdir -p "$rt_dir"
-chown ridetheln-system "$rt_dir"
-chgrp ridetheln-system "$rt_dir"
-mkdir -p "$rt_dir/sso"
-chown ridetheln-system "$rt_dir/sso"
-chgrp ridetheln-system-sso "$rt_dir/sso"
-chmod 750 "$rt_dir/sso"
-
 if [ -f "$rt_file" ];
 then
 	jq -s 'reduce .[] as $item ({}; . * $item)' "$rt_file" "$cfg_file" > "$rt_temp" || exit 1
