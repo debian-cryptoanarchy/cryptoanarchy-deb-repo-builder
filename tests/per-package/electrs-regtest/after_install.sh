8eac65fe7cd2d80e0399a4107eff5ad6a3244a61
Hollow Man <hollowman@opensuse.org>
2022-03-30 15:33:22 +0800

    Try more times to test connecting to electrs-regtest
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- tests/per-package/electrs-regtest/after_install.sh
+++ tests/per-package/electrs-regtest/after_install.sh
@@ -14,4 +14,12 @@ sleep 10
 electrs_port="`grep '^ *electrum_rpc_addr *= *' "/etc/electrs-$bitcoin_network/conf.d/interface.toml"`"
 electrs_port="`echo "$electrs_port" | sed 's/^ *electrum_rpc_addr *= *"[^:"]*:\([^"]*\)" *$/\1/'`"
 
-echo '{ "id" : 0, "method": "server.version", "params": ["1.9.5", "0.10"] }' | nc -q 10 127.0.0.1 "$electrs_port" >&2
+for i in {0..3}; do
+	if echo '{ "id" : 0, "method": "server.version", "params": ["1.9.5", "0.10"] }' | nc -q 10 127.0.0.1 "$electrs_port" >&2; then
+		break
+	elif [ $i -eq 3 ]; then
+		echo "Failed to connect to electrs-regtest"
+		exit 1
+	fi
+	sleep 2
+done
