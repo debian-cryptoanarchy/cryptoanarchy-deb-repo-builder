dc49fca83fd3169be02e05f61a33e7d7f1402088
Martin Habovstiak <martin.habovstiak@gmail.com>
2024-07-18 22:58:57 +0200

    Fix `BUILD_DIR` in CI
    The authoritative build machine use `/home/user/cadr-build`, not
    `/home/user/cadr-build/build`. This was probably source of
    non-determinism.

--- ci/build.sh
+++ ci/build.sh
@@ -3,9 +3,11 @@
 set -ex
 
 BUILD_DIR=/home/user/cadr-build
+REPO_DIR=/home/user/cryptoanarchy-deb-repo-builder
 sudo apt-get update
-sudo chown -R user $BUILD_DIR
-cd $BUILD_DIR
-MKCMD="make SOURCES="$1" BUILD_DIR=${BUILD_DIR}/build"
+sudo chown -R user "$REPO_DIR"
+cd "$REPO_DIR"
+MKCMD="make SOURCES="$1" BUILD_DIR=${BUILD_DIR}"
 $MKCMD build-dep
 $MKCMD all
+mv "$BUILD_DIR"/*.deb "$REPO_DIR/packages"
