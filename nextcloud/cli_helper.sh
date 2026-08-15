fe3bcd65414e1ab9f497b9326bccc6dda7ed0e46
Martin Habovstiak <martin.habovstiak@gmail.com>
2026-08-15 10:30:29 -0300

    Improve robustness of nextcloud cli_helper
    When a package `foo` version 1 depends on package `bar` version 1 such
    that it is NOT compatible with `bar` version 2 and `foo` version 2
    depends on `bar` version 2, dpkg/apt do not ensure that prerm of `foo`
    runs before `bar` upgrades even though it might be expected. This
    specific scenario is what we have: nextcloud depending on specific PHP
    version range. In such case, the upgrade script would become broken as
    it tries to call into new PHP unconditionally while still having the old
    script.
    
    To avoid upgrade failure, we need to give up on trying to run it when
    this happens. This change adds version detection to the `cli_helper`
    script which just logs and skips running the command when ran from
    upgrade or failed-upgrade maintainer script and detecting version
    mismatch. This is required to prepare the upgrade for next version.
    (Technically, maybe not required but leaving it for prerm of new version
    is not great.)

--- nextcloud/cli_helper.sh
+++ nextcloud/cli_helper.sh
@@ -2,8 +2,30 @@
 
 # Runs PHP with added settings from FPM pool configuration file in order to stay in sync
 
-php_version="`dpkg-query --showformat '${Version}' -W php-fpm | sed -e 's/^2://' -e 's/[+-].*$//'`"
-extra_args="`grep '^php_value\[' "/etc/php/$php_version/fpm/pool.d/nextcloud-server-system.conf" | sed 's/^php_value\[\([^]]*\)\] *= *\([^ ].*\)$/-d \1=\2/'`"
+function check_php_version() {
+	if ! dpkg --compare-versions "$1" "$2" "$3"; then
+		if [ "$MAINTSCRIPT_ACTION" = "upgrade" ] || [ "$MAINTSCRIPT_ACTION" = "failed-upgrade" ]; then
+			echo "$4">&2
+			exit 0
+		else
+			echo "Incompatible PHP version installed - the following condition failed: $1 $2 $3"
+			exit 1
+		fi
+	fi
+}
+
+php_package_version="`dpkg-query --showformat '${Version}' -W php-cli`" || exit 1
+nextcloud_deps="`dpkg-query --showformat '${Depends}' -W nextcloud-server`" || exit 1
+
+min_php_version="`echo $nextcloud_deps | sed 's/.*php-cli (>= \([^)]*\)).*/\1/'`" || exit 1
+check_php_version "$php_package_version" ge "$min_php_version" "The nextcloud-server package was upgraded before upgrading PHP - skipping maintainer script action $*"
+
+incompatible_php_version="`echo $nextcloud_deps | sed 's/.*php-cli (<< \([^)]*\)).*/\1/'`" || exit 1
+check_php_version "$php_package_version" lt "$incompatible_php_version" "PHP was upgraded before upgrading nextcloud-server - skipping maintainer script action $*"
+
+php_fpm_version="`dpkg-query --showformat '${Version}' -W php-fpm | sed -e 's/^2://' -e 's/[+-].*$//'`" || exit 1
+
+extra_args="`grep '^php_value\[' "/etc/php/$php_fpm_version/fpm/pool.d/nextcloud-server-system.conf" | sed 's/^php_value\[\([^]]*\)\] *= *\([^ ].*\)$/-d \1=\2/'`"
 
 export NEXTCLOUD_CONFIG_DIR=/var/lib/nextcloud-server-system/config
 export PHP_MEMORY_LIMIT=512M
