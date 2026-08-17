be637888f7ccc394b885d499aa3d0d8e0a9f4c63
Martin Habovstiak <martin.habovstiak@gmail.com>
2026-08-17 10:49:41 -0300

    Improve robustness of cli_helper even more
    In the previous commit detection of version mismatch was added. However,
    that was not sufficient: sometimes a dependency would be unpacked but
    not configured causing the same problem as with wrong version.
    
    This commit in addition detects any missing PHP dependencies and skips
    prerm if they are missing.

--- nextcloud/cli_helper.sh
+++ nextcloud/cli_helper.sh
@@ -3,6 +3,11 @@
 # Runs PHP with added settings from FPM pool configuration file in order to stay in sync
 
 function check_php_version() {
+	if [ -z "$1" ];
+	then
+		return 0
+	fi
+
 	if ! dpkg --compare-versions "$1" "$2" "$3"; then
 		if [ "$MAINTSCRIPT_ACTION" = "upgrade" ] || [ "$MAINTSCRIPT_ACTION" = "failed-upgrade" ]; then
 			echo "$4">&2
@@ -14,14 +19,33 @@ function check_php_version() {
 	fi
 }
 
-php_package_version="`dpkg-query --showformat '${Version}' -W php-cli`" || exit 1
+function check_dep_installed() {
+	if [ "`dpkg-query --showformat '${db:Status-Status}' -W "$1"`" '!=' "installed" ];
+	then
+		if [ "$MAINTSCRIPT_ACTION" = "upgrade" ] || [ "$MAINTSCRIPT_ACTION" = "failed-upgrade" ]; then
+			echo "The dependency $1 of nextcloud-server is not installed, skipping maintainer script action $*">&2
+			exit 0
+		else
+			echo "The dependency $1 of nextcloud-server is not installed">&2
+			exit 1
+		fi
+	fi
+}
+
 nextcloud_deps="`dpkg-query --showformat '${Depends}' -W nextcloud-server`" || exit 1
 
-min_php_version="`echo $nextcloud_deps | sed 's/.*php-cli (>= \([^)]*\)).*/\1/'`" || exit 1
-check_php_version "$php_package_version" ge "$min_php_version" "The nextcloud-server package was upgraded before upgrading PHP - skipping maintainer script action $*"
+php_deps="`echo "$nextcloud_deps" | tr ',' '\n' | grep '^php.*' | sed 's/ (.*)//'`"
 
-incompatible_php_version="`echo $nextcloud_deps | sed 's/.*php-cli (<< \([^)]*\)).*/\1/'`" || exit 1
-check_php_version "$php_package_version" lt "$incompatible_php_version" "PHP was upgraded before upgrading nextcloud-server - skipping maintainer script action $*"
+for dep in $php_deps;
+do
+	check_dep_installed "$dep"
+	php_package_version="`dpkg-query --showformat '${Version}' -W "$dep"`" || exit 1
+	min_php_version="`echo $nextcloud_deps | sed -n 's/.*'"$dep"' (>= \([^)]*\)).*/\1/p'`" || exit 1
+	check_php_version "$php_package_version" ge "$min_php_version" "The nextcloud-server package was upgraded before upgrading PHP - skipping maintainer script action $*"
+
+	incompatible_php_version="`echo $nextcloud_deps | sed -n 's/.*'"$dep"' (<< \([^)]*\)).*/\1/p'`" || exit 1
+	check_php_version "$php_package_version" lt "$incompatible_php_version" "PHP was upgraded before upgrading nextcloud-server - skipping maintainer script action $*"
+done
 
 php_fpm_version="`dpkg-query --showformat '${Version}' -W php-fpm | sed -e 's/^2://' -e 's/[+-].*$//'`" || exit 1
 
